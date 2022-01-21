594b7be92c47701bef1aa8a2bee26abea3f22c5f
jyeun722 <skqhs7276@gmail.com>
2022-01-21 22:31:57 +0900

    Build relationship to complete User Profile

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,6 +1,7 @@
 import User from "../models/User";
 import fetch from "node-fetch";
 import bcrypt from "bcrypt";
+import Video from "../models/Video";
 
 export const getJoin = (req, res) => res.render("Join", { pageTitle: "Join" });
 export const postJoin = async(req, res) => {
@@ -9,7 +10,6 @@ export const postJoin = async(req, res) => {
     if (password !== password2) {
         return res.status(400).render("Join", { pageTitle, errorMessage: "Password confirmation does not match",
      });
-    
     }
     const exists = await User.exists({ $or: [{ username }, { email }] });
     if (exists) {
@@ -128,5 +128,60 @@ export const logout = (req, res) => {
     return res.redirect("/");
 };
 
-export const edit = (req, res) => res.send("Edit User");
-export const see = (req, res) => res.send("See User");
+
+export const getEdit = (req, res) => {
+    return res.render("edit-profile", { pageTitle: "Edit Profile" });
+};
+
+export const postEdit = async(req, res) => {
+    const {
+        session: {
+            user: { _id, avatarUrl },
+        },
+        body: { name, email, username, location },
+        file,
+    } = req;
+    const updateUser = await User.findByIdAndUpdate(_id, {
+        avatarUrl: file ? file.path : avatarUrl,
+        name, email, username, location,
+    }, { new: true });
+    req.session.user = updateUser;
+    return res.redirect("/users/edit");
+};
+
+export const getChangePassword = async(req, res) => {
+    if (req.session.user.socialOnly === true) {
+        return res.redirect("/");
+    }
+    return res.render("users/change-password", {pageTitle:"Change Password"})
+}
+export const postChangePassword = async(req, res) => {
+    const {
+        session: {
+            user: { _id},
+        },
+        body: { oldPassword, newPassword, newPasswordConfirmation },
+    } = req;
+    const user = await User.findById(_id);
+    const ok = await bcrypt.compare(oldPassword, user.password);
+    if (!ok) {
+        return res.status(400).render("users/change-password",
+        {pageTitle:"Change Password", errorMessage: "The current password is incorrect",})
+    }
+    if (newPassword !== newPasswordConfirmation) {
+        return res.status(400).render("users/change-password",
+        {pageTitle:"Change Password", errorMessage: "The password does not match the confirmation",})
+    }
+    user.password = newPassword;
+    await user.save();
+    return res.redirect("/users/logout");
+}
+
+export const see = async(req, res) => {
+    const { id } = req.params;
+    const user = await User.findById(id).populate("videos");
+    if(!user) {
+        return res.status(400).render("404", { pageTitle: "User not found." });
+    }
+    return res.render("users/profile", { pageTitle: user.name, user, });
+};
--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,13 +1,26 @@
 import express from "express";
-import { see, logout } from "../controllers/userController";
-import { edit, startGithubLogin, finishGithubLogin } from "../controllers/userController";
+import { see, logout, postChangePassword } from "../controllers/userController";
+import { getEdit, postEdit, startGithubLogin, finishGithubLogin, getChangePassword } from "../controllers/userController";
+import { avatarUpload, protectorMiddleware, publicOnlyMiddleware } from "../middlewares";
 
 const userRouter = express.Router();
 
-userRouter.get("/logout", logout);
-userRouter.get("/edit", edit);
-userRouter.get("/github/start", startGithubLogin);
-userRouter.get("/github/finish", finishGithubLogin);
+userRouter.get("/logout", protectorMiddleware,  logout);
+userRouter
+    .route("/edit")
+    .all(protectorMiddleware)
+    .get(getEdit)
+    .post(avatarUpload.single("avatar"), postEdit);
+    
+userRouter
+    .route("/change-password")
+    .all(protectorMiddleware)
+    .get(getChangePassword)
+    .post(postChangePassword);
+
+userRouter.get("/github/start", publicOnlyMiddleware, startGithubLogin);
+userRouter.get("/github/finish", publicOnlyMiddleware, finishGithubLogin);
+
 userRouter.get("/:id", see);
 
 export default userRouter;
